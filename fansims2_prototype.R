9f1cf7dd2d585e9e92405d1a5370a69d9cd240a7
mcarlo <tom.anichini@gmail.com>
2015-07-09 17:12:26 -0700

    Improved factorization
    Upset strategy results coming out zero in display

--- fansims2_prototype.R
+++ fansims2_prototype.R
@@ -3,10 +3,11 @@
 # Set directory and read in files
 #
 rm(list = ls())
+#setupComplete = FALSE
 
-set_up <- function(weekFileName){
+set_up <- function(weekFilename){
   setwd("C:/Users/Anichini/Documents") # setwd("D:/WTP")
-  weekFile <- read.csv(weekFileName, stringsAsFactors = F)
+  weekFile <- read.csv(weekFilename, stringsAsFactors = F)
   weekFile <<- weekFile[order(-weekFile$Confidence),]
   winProb <<- weekFile[, 2]
   if (max(winProb) > 1) {winProb <<- winProb/100.0}
@@ -45,20 +46,20 @@ initialize_matrices <- function(){
 
   premiumPts <<- 0 + prem * premium
 
-  oneupsetmatrix <<- matrix(rep(1, games**2), nrow = games, ncol = games)
-  diag(oneupsetmatrix) <<- rep(0, games)
+  oneupsetmatrix <- matrix(rep(1, games**2), nrow = games, ncol = games)
+  diag(oneupsetmatrix) <- rep(0, games)
 
-  oneupsetConfidence <<- oneupsetmatrix
+  oneupsetConfidence <- oneupsetmatrix
   for (j in 1:games){
     for (i in 1:games){
       if (i < j) {
-        oneupsetConfidence[i, j] <<- games + premiumPts - i
+        oneupsetConfidence[i, j] <- games + premiumPts - i
       } else {
-        oneupsetConfidence[i, j] <<- games + premiumPts - i + 1
+        oneupsetConfidence[i, j] <- games + premiumPts - i + 1
       }
     }
   }
-  diag(oneupsetConfidence) <<- rep(games + premiumPts, games)
+  diag(oneupsetConfidence) <- rep(games + premiumPts, games)
 
   set.seed(123) #as.numeric(Sys.time()))
 
@@ -68,7 +69,7 @@ initialize_matrices <- function(){
   # simPicks[1:10, 1:10]
 
   simRand <- matrix(runif(games*playerCols), nrow = games, ncol = playerCols)
-  simRanks <- matrix(rep(0, games*playerCols), nrow = games, ncol = playerCols)
+  # simRanks <<- matrix(rep(0, games*playerCols), nrow = games, ncol = playerCols)
   # simRanks[1:10, 1:10]
 
   simFavs <- matrix(qbinom(simRand, games, (favConf - .5)/games, lower.tail = T), nrow = games, ncol = playerCols) + (runif(playerCols * games) - .5)
@@ -79,26 +80,30 @@ initialize_matrices <- function(){
 
   simRaw <- (simPrior + simFavs *simPicks + simDogs *(1 - simPicks))/2
 
-  simRanks <<- apply(simRaw, 2, rank) + premiumPts # max(apply(simRanks, 2, max))
+  simRanks <- apply(simRaw, 2, rank) + premiumPts # max(apply(simRanks, 2, max))
 
-  simOutcomes <<- 1*(runif(games) <= winProb); # simOutcomes[5] <- 0
-  myRanks <<- rank(winProb)+premiumPts
+  #simOutcomes <- 1*(runif(games) <= winProb); # simOutcomes[5] <- 0
+  simOutcomes2 <<- matrix(1*(runif(games * 10000) <= winProb), nrow = games, ncol = 10000)
 
-  myCorrect <<- simOutcomes * myRanks
-  myPoints <<- sum(myCorrect)
-  simCorrect <<- simOutcomes * simPicks + (1 - simOutcomes) * (1 - simPicks)
+  myRanks <- rank(winProb)+premiumPts
 
-  totalPoints <<- apply(simCorrect * simRanks, 2, sum)
-  # totalPoints[1:10]
+  myCorrect <- t(myRanks) %*% simOutcomes2 # * myRanks
+  myPoints <<- myCorrect
 
-  upsetPoints <<- apply(((simOutcomes * oneupsetmatrix) + (simOutcomes - 1) * (oneupsetmatrix - 1)) * oneupsetConfidence, 2, sum)
+  simCorrect <- t(t(simPicks * simRanks) %*% simOutcomes2 + t((1 - simPicks) * simRanks) %*% (1 - simOutcomes2))
+
+  upsetPoints <<- t(t(oneupsetmatrix * oneupsetConfidence) %*% simOutcomes2 + t((1 - oneupsetmatrix) * oneupsetConfidence) %*% (simOutcomes2 - 1))
   # upsetPoints[1:10]
 
-  }
+  totalPoints <<- simCorrect
+  #max(totalPoints)
+
+  setupComplete = TRUE
+}
 
 runIterations <- function(maxIter, numFans = 100){
 
-  stratWins <<- rep(0, 11)
+  stratWins <<- rep(0.0, 11)
   stratPlace <<- stratWins
   stratShow <<- stratWins
 
@@ -106,62 +111,67 @@ runIterations <- function(maxIter, numFans = 100){
   tempPlace <- temp
   tempShow <- temp
 
-  set.seed(123)
+  # set.seed(123)
 
-  for (i in 1:1){ # i = 1
-    for (j in 1:1){ # j = 1
-      fanIndex <- sample(1:10000, numFans, replace = T)
+  for (i in 1:maxIter){ # i = 11; numFans = 90
+    set.seed(i)
 
-      totalPointsIter <- totalPoints[fanIndex]
-      WTP <- 1*(myPoints >= max(totalPointsIter))
-      opp1Win <- 1*(upsetPoints[1] > max(totalPointsIter))
-      opp2Win <- 1*(upsetPoints[2] > max(totalPointsIter))
-      opp3Win <- 1*(upsetPoints[3] > max(totalPointsIter))
-      opp4Win <- 1*(upsetPoints[4] > max(totalPointsIter))
-      opp5Win <- 1*(upsetPoints[5] > max(totalPointsIter))
+    resultIndex <- sample(1:10000, 1) # resultIndex = 9947
+    fanIndex <- sample(1:10000, numFans, replace = T)
+    #simOutcomes2[, resultIndex]
 
-      opp6Win <- 1*(upsetPoints[6] > max(totalPointsIter))
-      opp7Win <- 1*(upsetPoints[7] > max(totalPointsIter))
-      opp8Win <- 1*(upsetPoints[8] > max(totalPointsIter))
-      opp9Win <- 1*(upsetPoints[9] > max(totalPointsIter))
-      opp10Win <- 1*(upsetPoints[10] > max(totalPointsIter))
+    # max(upsetPoints[resultIndex, ])
+    # fanIndex <- which(totalPoints[resultIndex, ] < max(upsetPoints[resultIndex, ]))[1:numFans]
+    totalPointsIter <- totalPoints[resultIndex, fanIndex]
 
-      WTPplace <- 1*(sum(myPoints < totalPointsIter) == 1)
-      opp1Place <- 1*(sum(upsetPoints[1] < totalPointsIter) == 1)
-      opp2Place <- 1*(sum(upsetPoints[2] < totalPointsIter) == 1)
-      opp3Place <- 1*(sum(upsetPoints[3] < totalPointsIter) == 1)
-      opp4Place <- 1*(sum(upsetPoints[4] < totalPointsIter) == 1)
-      opp5Place <- 1*(sum(upsetPoints[5] < totalPointsIter) == 1)
+    WTP <- 1*(myPoints[, resultIndex] >= max(totalPointsIter))
+    opp1Win <- 1*(upsetPoints[resultIndex, 1] > max(totalPointsIter))
+    opp2Win <- 1*(upsetPoints[resultIndex, 2] > max(totalPointsIter))
+    opp3Win <- 1*(upsetPoints[resultIndex, 3] > max(totalPointsIter))
+    opp4Win <- 1*(upsetPoints[resultIndex, 4] > max(totalPointsIter))
+    opp5Win <- 1*(upsetPoints[resultIndex, 5] > max(totalPointsIter))
 
-      opp6Place <- 1*(sum(upsetPoints[6] < totalPointsIter) == 1)
-      opp7Place <- 1*(sum(upsetPoints[7] < totalPointsIter) == 1)
-      opp8Place <- 1*(sum(upsetPoints[8] < totalPointsIter) == 1)
-      opp9Place <- 1*(sum(upsetPoints[9] < totalPointsIter) == 1)
-      opp10Place <- 1*(sum(upsetPoints[10] < totalPointsIter) == 1)
+    opp6Win <- 1*(upsetPoints[resultIndex, 6] > max(totalPointsIter))
+    opp7Win <- 1*(upsetPoints[resultIndex, 7] > max(totalPointsIter))
+    opp8Win <- 1*(upsetPoints[resultIndex, 8] > max(totalPointsIter))
+    opp9Win <- 1*(upsetPoints[resultIndex, 9] > max(totalPointsIter))
+    opp10Win <- 1*(upsetPoints[resultIndex, 10] > max(totalPointsIter))
 
-      WTPShow <- 1*(sum(myPoints < totalPointsIter) == 2)
-      opp1Show <- 1*(sum(upsetPoints[1] < totalPointsIter) == 2)
-      opp2Show <- 1*(sum(upsetPoints[2] < totalPointsIter) == 2)
-      opp3Show <- 1*(sum(upsetPoints[3] < totalPointsIter) == 2)
-      opp4Show <- 1*(sum(upsetPoints[4] < totalPointsIter) == 2)
-      opp5Show <- 1*(sum(upsetPoints[5] < totalPointsIter) == 2)
+    WTPplace <- 1*(sum(myPoints[, resultIndex] < totalPointsIter) == 1)
+    opp1Place <- 1*(sum(upsetPoints[resultIndex, 1] < totalPointsIter) == 1)
+    opp2Place <- 1*(sum(upsetPoints[resultIndex, 2] < totalPointsIter) == 1)
+    opp3Place <- 1*(sum(upsetPoints[resultIndex, 3] < totalPointsIter) == 1)
+    opp4Place <- 1*(sum(upsetPoints[resultIndex, 4] < totalPointsIter) == 1)
+    opp5Place <- 1*(sum(upsetPoints[resultIndex, 5] < totalPointsIter) == 1)
 
-      opp6Show <- 1*(sum(upsetPoints[6] < totalPointsIter) == 2)
-      opp7Show <- 1*(sum(upsetPoints[7] < totalPointsIter) == 2)
-      opp8Show <- 1*(sum(upsetPoints[8] < totalPointsIter) == 2)
-      opp9Show <- 1*(sum(upsetPoints[9] < totalPointsIter) == 2)
-      opp10Show <- 1*(sum(upsetPoints[10] < totalPointsIter) == 2)
+    opp6Place <- 1*(sum(upsetPoints[resultIndex, 6] < totalPointsIter) == 1)
+    opp7Place <- 1*(sum(upsetPoints[resultIndex, 7] < totalPointsIter) == 1)
+    opp8Place <- 1*(sum(upsetPoints[resultIndex, 8] < totalPointsIter) == 1)
+    opp9Place <- 1*(sum(upsetPoints[resultIndex, 9] < totalPointsIter) == 1)
+    opp10Place <- 1*(sum(upsetPoints[resultIndex, 10] < totalPointsIter) == 1)
 
-      temp <- temp + c(WTP, opp1Win, opp2Win, opp3Win, opp4Win, opp5Win, opp6Win, opp7Win, opp8Win, opp9Win, opp10Win)
+    WTPShow <- 1*(sum(myPoints[, resultIndex] < totalPointsIter) == 2)
+    opp1Show <- 1*(sum(upsetPoints[resultIndex, 1] < totalPointsIter) == 2)
+    opp2Show <- 1*(sum(upsetPoints[resultIndex, 2] < totalPointsIter) == 2)
+    opp3Show <- 1*(sum(upsetPoints[resultIndex, 3] < totalPointsIter) == 2)
+    opp4Show <- 1*(sum(upsetPoints[resultIndex, 4] < totalPointsIter) == 2)
+    opp5Show <- 1*(sum(upsetPoints[resultIndex, 5] < totalPointsIter) == 2)
 
-      tempPlace <- tempPlace + c(WTPplace, opp1Place, opp2Place, opp3Place, opp4Place,
-                     opp5Place, opp6Place, opp7Place, opp8Place, opp9Place,
-                     opp10Place)
+    opp6Show <- 1*(sum(upsetPoints[resultIndex, 6] < totalPointsIter) == 2)
+    opp7Show <- 1*(sum(upsetPoints[resultIndex, 7] < totalPointsIter) == 2)
+    opp8Show <- 1*(sum(upsetPoints[resultIndex, 8] < totalPointsIter) == 2)
+    opp9Show <- 1*(sum(upsetPoints[resultIndex, 9] < totalPointsIter) == 2)
+    opp10Show <- 1*(sum(upsetPoints[resultIndex, 10] < totalPointsIter) == 2)
 
-      tempShow <- tempShow + c(WTPShow, opp1Show, opp2Show, opp3Show, opp4Show, opp5Show,
-                    opp6Show, opp7Show, opp8Show, opp9Show, opp10Show)
+    temp <- temp + c(WTP, opp1Win, opp2Win, opp3Win, opp4Win, opp5Win, opp6Win, opp7Win, opp8Win, opp9Win, opp10Win)
 
-    }
+    tempPlace <- tempPlace + c(WTPplace, opp1Place, opp2Place, opp3Place, opp4Place,
+                   opp5Place, opp6Place, opp7Place, opp8Place, opp9Place,
+                   opp10Place)
+
+    tempShow <- tempShow + c(WTPShow, opp1Show, opp2Show, opp3Show, opp4Show, opp5Show,
+                  opp6Show, opp7Show, opp8Show, opp9Show, opp10Show)
+
     stratWins <<- stratWins + temp
     stratPlace <<- stratPlace + tempPlace
     stratShow <<- stratShow + tempShow
@@ -171,28 +181,38 @@ runIterations <- function(maxIter, numFans = 100){
     tempShow <- temp
 
   }
+  stratWins <<- stratWins
+  stratPlace <<- stratPlace
+  stratShow <<- stratShow
 }
 
 displayResults <- function(group, maxIter){
-  resultsMatrix <- as.matrix(cbind(stratWins, stratPlace, stratShow), nrow = 6, ncol = 3)
-  winnings <- as.data.frame(t((resultsMatrix %*% payouts))/(1.0 * maxIter * maxIter))
+  resultsMatrix <- as.matrix(cbind(stratWins, stratPlace, stratShow), nrow = 6, ncol = 3) * 17.0 / maxIter
+  winnings <- as.data.frame(t((resultsMatrix %*% payouts)))
   colnames(winnings) <- c("WTP", "Fav", "Fav-1", "Fav-2", "Fav-3", "Fav-4",
                           "Fav-5", "Fav-6", "Fav-7", "Fav-8", "Fav-9")
   rownames(resultsMatrix) <- colnames(winnings)
-  print(resultsMatrix)
+  #print(resultsMatrix)
   print(round(winnings, 2))
   print(apply(resultsMatrix, 1, sum))
 }
 
-simWeeklyPayouts <- function(weekFilename = "2014week11.csv", maxIter = 25, group = "Jack", numFans = 190){
+simWeeklyPayouts <- function(weekFilename = "2014week14.csv", maxIter = 2000, group = "Ted", numFans = 90){ # weekFilename = "2014week14.csv"; maxIter = 2000; group = "Red"; numFans = 90
 
   set_up(weekFilename)
   define_payouts(group)
-  initialize_matrices()
+  initialize_matrices() # simOutcomes2
   runIterations(maxIter, numFans = numFans)
   displayResults(group, maxIter)
 }
 
-system.time(simWeeklyPayouts())
-system.time(simWeeklyPayouts(group = "Ted"))
-system.time(simWeeklyPayouts(group = "Jack"))
+system.time(simWeeklyPayouts(weekFilename = "2014week10.csv"))
+system.time(simWeeklyPayouts(weekFilename = "2014week11.csv"))
+system.time(simWeeklyPayouts(weekFilename = "2014week12.csv"))
+system.time(simWeeklyPayouts(weekFilename = "2014week14.csv"))
+system.time(simWeeklyPayouts(weekFilename = "2014week15.csv"))
+system.time(simWeeklyPayouts(weekFilename = "2014week16.csv"))
+system.time(simWeeklyPayouts(weekFilename = "2014week17.csv"))
+
+system.time(simWeeklyPayouts(weekFilename = "2014week11.csv", group = "Ted", numFans = 100))
+system.time(simWeeklyPayouts(weekFilename = "2014week11.csv", group = "Jack", numFans = 190))
